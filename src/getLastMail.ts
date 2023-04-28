c21b6e0a948dc3832983b076711b1235b2fba3ed
Chun Wai Lok <cwlok0613@gmail.com>
2023-04-28 15:27:53 +0800

    Add npx command for get last mail

--- src/getLastMail.ts
+++ src/getLastMail.ts
@@ -1,13 +1,14 @@
-import { Mail } from "./constant";
 import { getContent, getMailList, login } from "./yopmail";
 
 const getLastMail = async (email: string): Promise<string | undefined> => {
-  const page = await login(email);
+  const [page, browser] = await login(email);
   const mails = await getMailList(page);
   const lastMail = mails[0];
   const { id } = lastMail;
 
-  return await getContent(page, id);
+  const content = await getContent(page, id);
+  browser.close();
+  return content;
 }
 
 export default getLastMail;
